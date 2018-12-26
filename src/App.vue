ebdd23c1e2ea02cd53bbc5e04bfe9d07e6c50976
kdev <kdev.software1110@gmail.com>
2018-12-26 10:04:57 -0500

    continuing on by styling our project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,37 @@
 <template>
   <div id="app">
-    <Skills />
+    <Skills/>
   </div>
 </template>
 
 <script>
-import Skills from './components/Skills.vue'
+import Skills from "./components/Skills.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     Skills
   }
-}
+};
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css?family=Montserrat:400,700");
+
 body {
   background-color: #eeeeee;
+  font-family: "Montserrat", sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body,
+html {
+  margin: 0;
+  height: 100%;
 }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 50%;
 }
 </style>
